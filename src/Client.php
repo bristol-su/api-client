efc6a652b8dd4c457efcd5cd917b0373b27d8156
Toby Twigger <tobytwigger1@gmail.com>
2020-08-21 21:56:31 +0100

    Updates

--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,19 @@
 <?php
 
+namespace BristolSU\ApiClient;
+
 class Client
 {
+
+    /**
+     * Call the underlying response class
+     *
+     * @param $name
+     * @param $arguments
+     */
+    public function __call($name, $arguments)
+    {
+        // Resolve out the container
+    }
 
 }
